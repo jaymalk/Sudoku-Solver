294e8afe7f55876060f79da2a2a691216a30a21b
Rajbir Malik <cs1170416@iitd.ac.in>
2019-04-03 10:58:30 +0530

    Added Sudoku Puzzle and Ignored Classes

---
+++ lib/Sudoku.java
@@ -0,0 +1,43 @@
+
+
+// Class for representing the sudoku board (and cells)
+public class Sudoku {
+    private int size;       // The dimensions of square grid, must be a perfect square
+    private int[][] grid;       // Representing cells' values
+    private boolean[][] set;    // Determining the status of cells
+
+    // Constructor
+    public Sudoku(int size) {
+        this.size = size;
+        // Initialising the grid
+        grid = new int[size][size];
+        set = new boolean[size][size];
+    }
+
+    // Get size
+    public int size() {
+        return this.size;
+    }
+
+    // Get grids
+    public int[][] grid() {
+        return this.grid;
+    }
+
+    // Get the set values
+    public boolean[][] set() {
+        return this.set;
+    }
+
+    // Draw sudoku on the terminal
+    public void CLI_Draw() {
+        for(int[] a : grid) {
+            for(int b : a)
+                if(b == 0)
+                    System.out.print(". ");
+                else
+                    System.out.print(b+" ");
+            System.out.println();
+        }
+    }
+}
